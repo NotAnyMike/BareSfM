dafc427622aaebd00907a29ec646a5dc698db349
MikeW <mikewoodcockc@gmail.com>
2020-02-28 17:27:44 +0100

    pointing test to right fake dataset

--- test/test_dataloader.py
+++ test/test_dataloader.py
@@ -4,11 +4,11 @@ from pdb import set_trace
 from dataloaders import Shapes3D_loader, read_lines, parse_lines
 
 def test_read_lines():
-    lines = read_lines("test/train_files.txt")
+    lines = read_lines("test/test_dataset/train_files.txt")
     assert lines == ["1 2", "3 4"]
 
 def test_shapes3d_dataloader():
-    dl = Shapes3D_loader(320, 320, 'test', True)
+    dl = Shapes3D_loader(320, 320, 'test/test_dataset', True)
     K = np.resize(np.arange(1, 17, dtype=np.float),(4, 4))
     assert dl.files == [('1', '2'), ('3', '4')]
     assert np.array_equal(dl.K, K)
@@ -19,9 +19,9 @@ def test_parse_lines():
 
 def test_reshape_and_totensor_transform():
     h, w = 320, 320
-    dl = Shapes3D_loader(h, w, 'test', True)
+    dl = Shapes3D_loader(h, w, 'test/test_dataset', True)
     assert dl[0][('color', 'a')].shape == (3, h, w)
 
     h, w = 100, 50
-    dl = Shapes3D_loader(h, w, 'test', True)
+    dl = Shapes3D_loader(h, w, 'test/test_dataset', True)
     assert dl[0][('color', 'c')].shape == (3, h, w)
